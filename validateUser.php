cc55c52f84be3286c24a881ce90b641d397b1f22
huangtao800 <huangtao7725@live.com>
2015-01-07 00:40:58 +0800

    fix conflicts again

--- validateUser.php
+++ validateUser.php
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<html>
-	<form action="http://218.94.159.104:6003/weixinMoodle/test.php" method="post">
-		<lable for="username">Please input username: </lable>
-		<input id="username" type="text" name="username" /> <br />
-		<label for="password">Please input your password: </label> 
-		<input id="password" type="text" name="password"> <br/>
-		<input type="submit" />
-	</form>
-</html>
-=======
 <?php
 if(isset($_POST['username'])&&isset($_POST['password'])){
 	$url = "http://218.94.159.104:6003/weixinMoodle/test.php";
@@ -32,4 +21,3 @@ function postData($url,$data){
 	return $result;
 }
 ?>
->>>>>>> f014043e863fc5b0eec28bb3f6a2c92919606106
