f17ac575f8b692489bbbbb48d477a28b0db37308
weixinMoodle <msn_ht@live.cn>
2015-01-06 23:36:10 +0800

    first commit

---
+++ validateUser.php
@@ -0,0 +1,9 @@
+<html>
+	<form action="http://218.94.159.104:6003/weixinMoodle/test.php" method="post">
+		<lable for="username">Please input username: </lable>
+		<input id="username" type="text" name="username" /> <br />
+		<label for="password">Please input your password: </label> 
+		<input id="password" type="text" name="password"> <br/>
+		<input type="submit" />
+	</form>
+</html>
