821338cef0ded2c4046306e3f5560fb9978c9e22
huangtao800 <huangtao7725@live.com>
2015-01-07 12:50:49 +0800

    fix error

--- validateUser.php
+++ validateUser.php
@@ -9,7 +9,7 @@
     $ch = curl_init();  
     curl_setopt($ch, CURLOPT_POST, 1);  
     curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CUROPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  
          
     $result = curl_exec($ch); //÷¥––post«Î«Û
