2b44188a232076b3646b988e4781b132f7fff02e
huangtao800 <huangtao7725@live.com>
2015-01-07 19:09:12 +0800

    add post method

--- validateUser.php
+++ validateUser.php
@@ -1,24 +1,24 @@
 <?php  
+    $username = $_POST["username"];
+    $password = $_POST["password"];
     $post_data =   
                 array(  
-                        'username=huangtao',  
-                        'password=Paul_1993',  
+                        "username = $username",  
+                        "password = $password",  
                 );  
       
         $post_data = implode('&',$post_data);  
       
-        $url='http://218/94/159/104:6004/weixinMoodle/test.php';  
+        $url='http://localhost:6004/weixinMoodle/test.php';  
       
         $ch = curl_init();  
         curl_setopt($ch, CURLOPT_POST, 1);  
         curl_setopt($ch, CURLOPT_URL,$url);  
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  
-        ob_start();  
-        curl_exec($ch);  
-        $result = ob_get_contents() ;  
-        ob_end_clean();  
+         
+        $result = curl_exec($ch);  
+        
       
     echo $result;  
       
-      
-    ?>  
+?>  
