0048f8e935cd744aef932d19af47de7b3e2fe2c8
huangtao800 <huangtao7725@live.com>
2015-01-07 19:18:42 +0800

    correct post methods

--- validateUser.php
+++ validateUser.php
@@ -1,22 +1,16 @@
 <?php  
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $post_data =   
-                array(  
-                        "username = $username",  
-                        "password = $password",  
-                );  
+    $post_data = "username=$username&password=$password";
       
-        $post_data = implode('&',$post_data);  
+    $url='http://localhost:6003/weixinMoodle/test.php';  
       
-        $url='http://localhost:6004/weixinMoodle/test.php';  
-      
-        $ch = curl_init();  
-        curl_setopt($ch, CURLOPT_POST, 1);  
-        curl_setopt($ch, CURLOPT_URL,$url);  
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  
+    $ch = curl_init();  
+    curl_setopt($ch, CURLOPT_POST, 1);  
+    curl_setopt($ch, CURLOPT_URL,$url);  
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);  
          
-        $result = curl_exec($ch);  
+    $result = curl_exec($ch);  
         
       
     echo $result;  
