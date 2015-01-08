fb7928fa4cd9a7266ea06f37f1f3d476a7386106
weixinMoodle <msn_ht@live.cn>
2015-01-08 23:54:44 +0800

    add password

--- userInput.php
+++ userInput.php
@@ -3,7 +3,7 @@
 		<lable for="username">Please input username: </lable>
 		<input id="username" type="text" name="username" /> <br />
 		<label for="password">Please input your password: </label> 
-		<input id="password" type="text" name="password"> <br/>
+		<input id="password" type="password" name="password"> <br/>
 		<input type="submit" />
 	</form>
 </html>
