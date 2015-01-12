b91cbadd6f6b78697578299883aee2de5439adfb
huangtao800 <huangtao7725@live.com>
2015-01-13 00:14:10 +0800

    modify

--- moodleInterface.php
+++ moodleInterface.php
@@ -1,29 +1,26 @@
 <?php
-
-require_once('./moodleClass.php');
-
-
-function getTeacherCourses($openid){
-	$url='http://localhost:6003/weixinMoodle/getTeacherCourses.php';
+require_once ('./moodleClass.php');
+function getTeacherCourses($openid) {
+	$url = 'http://localhost:6003/weixinMoodle/getTeacherCourses.php';
 	$post_data = "openid=$openid";
 	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_URL,$url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+	$ch = curl_init ();
+	curl_setopt ( $ch, CURLOPT_POST, 1 );
+	curl_setopt ( $ch, CURLOPT_URL, $url );
+	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
 	
-	$result = curl_exec($ch);
+	$result = curl_exec ( $ch );
 	curl_close ( $ch );
 	
-//	$result = "[" . $result . "]";
-	$result = json_decode($result);
-// 	$result = (Array)$result;
+	$result = json_decode ( $result );
 	
-// 	foreach($result as $r=>$r_value){
-// 		echo $r_value->fullname;
-//	}
-	echo $result[0]->fullname;	
+	for($x = 0; $x < count($result); $x ++) {
+		$courseId = $result[$x]->id;
+		$courseName = $result[$x]->fullname;
+		echo $courseId;
+		echo $courseName;
+	}
 }
-getTeacherCourses("1");
+getTeacherCourses ( "1" );
 ?>
