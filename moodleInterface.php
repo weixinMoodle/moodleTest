140944f327d0408a3a3032a2342ef238a0285daf
huangtao800 <huangtao7725@live.com>
2015-01-13 12:31:01 +0800

    encoding

--- moodleInterface.php
+++ moodleInterface.php
@@ -55,6 +55,11 @@ function getCourseNameById($courseId){
 	$post_data = "courseId=$courseId";
 
 	$ch = curl_init ();
+	$this_header = array(
+			"content-type: application/x-www-form-urlencoded;
+			charset=UTF-8"
+	);
+	curl_setopt ( $ch,CURLOPT_HTTPHEADER,$this_header);
 	curl_setopt ( $ch, CURLOPT_POST, 1 );
 	curl_setopt ( $ch, CURLOPT_URL, $url );
 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
