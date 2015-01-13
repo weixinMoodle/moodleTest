e938eb9f1a4b24c2e19bf339dc3e0cd867842bd5
huangtao800 <huangtao7725@live.com>
2015-01-13 12:17:06 +0800

    Add getCourseNameById

--- moodleInterface.php
+++ moodleInterface.php
@@ -50,4 +50,20 @@ function getCourseIdByKeyword($keyword){
 	return $result;
 }
 
+function getCourseNameById($courseId){
+	$url = 'http://localhost:6003/weixinMoodle/getCourseNameById.php';
+	$post_data = "courseId=$courseId";
+
+	$ch = curl_init ();
+	curl_setopt ( $ch, CURLOPT_POST, 1 );
+	curl_setopt ( $ch, CURLOPT_URL, $url );
+	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
+
+	$response = curl_exec ( $ch );
+	curl_close($ch);
+
+	return $result;
+}
+
 ?>
