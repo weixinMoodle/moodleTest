a94134fe35c34863829cd6d1be74a7bd48cbf71a
huangtao800 <huangtao7725@live.com>
2015-01-14 10:37:51 +0800

    test not exist course

--- test/testGetCourseNameById.php
+++ test/testGetCourseNameById.php
@@ -5,7 +5,7 @@
 
 include (dirname(__FILE__) . '/../moodleInterface.php');
 
-$result = getCourseNameById('3');
+$result = getCourseNameById('100');
 
 if(count($result) == 0){
 	echo "Course not found";
